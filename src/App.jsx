d0cb174958ade802c79b79207f29d2cc3ff7191a
Gajaa-Azhura <shinr433@gmail.com>
2025-04-23 16:11:17 +0530

    my first command

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from '../src/components/Navbar'
+import Aboutus from './pages/Aboutus'
+import Faq from './pages/Faq'
+import Home from './pages/Home'
+import Contactus from './pages/Contactus'
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Aboutus />
+    <Faq />
+    <Contactus />
+    </>
+  )
+}
+
+export default App
